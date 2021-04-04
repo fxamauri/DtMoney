0b1f3609bdb868150cf22a537a060757c9facbcb
amauri dias <athlon.amauri@gmail.com>
2021-04-04 02:40:56 -0300

    fill table transactions with data transactions from api

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -3,11 +3,21 @@ import { api } from '../../services/api';
 
 import { Container } from './styles';
 
+interface Transaction {
+    id: number,
+    title: string,
+    amount: number,
+    type: string,
+    category: string,
+    createdAt: string,
+}
+
 export function TransactionsTable() {
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
     useEffect(() => {
         api('transactions')
             .then(response => {
-                console.log('tes', response.data)
+                setTransactions(response.data.transactions);
             })
     }, []);
     return (
@@ -22,24 +32,21 @@ export function TransactionsTable() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Desenvolvimento de website</td>
-                        <td>R$ 12.000,00</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
+                {transactions.map(transaction => (
+                    <tr key={transaction.id}>
+                        <td>{transaction.title}</td>
+                        <td className={transaction.type}>
+                            {new Intl.NumberFormat('pt-BR', {
+                                style: 'currency',
+                                currency: 'BRL'
+                            }).format(transaction.amount)}
+                        </td>
+                        <td>{transaction.category}</td>
+                        <td>
+                            {new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}
+                        </td>
                     </tr>
-                    <tr>
-                        <td>Desenvolvimento de website</td>
-                        <td className="deposit">R$ 12.000,00</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Desenvolvimento de website</td>
-                        <td className="withdraw">R$ - 1.000,00</td>
-                        <td>Casa</td>
-                        <td>17/02/2021</td>
-                    </tr>
+                ))}
                 </tbody>
             </table>
         </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     models: {
       transaction: Model
     },
+
+    seeds(server) {
+      server.db.loadData({
+          transactions: [
+              {
+                  id: 1,
+                  title: 'Freelancer de website',
+                  type: 'deposit',
+                  category: 'Dev',
+                  amount: 10000,
+                  createdAt: new Date('2021-02-12 09:00:00')
+              },
+              {
+                  id: 2,
+                  title: 'Alguel',
+                  type: 'withdraw',
+                  category: 'Casa',
+                  amount: 1100,
+                  createdAt: new Date('2021-02-14 15:10:50')
+              }
+          ]
+      })
+    },
     routes() {
         this.namespace = 'api';
 
